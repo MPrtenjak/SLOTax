8194f620bb3cf74f51b71bcce73fbc1d5faf6deb
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-08 19:20:27 +0200

    .

--- SLOTaxGuiTest/MainWindow.xaml.cs
+++ SLOTaxGuiTest/MainWindow.xaml.cs
@@ -4,16 +4,35 @@
 // <author>Matjaz Prtenjak</author>
 //-----------------------------------------------------------------------
 
+using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows;
+using MNet.SLOTaxService.Services;
 
 namespace SLOTaxGuiTest
 {
   public partial class MainWindow : Window, INotifyPropertyChanged
   {
+    public ObservableCollection<X509Certificate2> certificates { get; set; }
+
     public MainWindow()
     {
       this.InitializeComponent();
+
+      this.init();
+    }
+
+    private void init()
+    {
+      Certificate cert = new Certificate();
+
+      this.certificates = new ObservableCollection<X509Certificate2>();
+      foreach (var certificate in cert.GetAllFursCertificates())
+      {
+        this.certificates.Add(certificate);
+      }
     }
 
     #region INotifyPropertyChanged Members
