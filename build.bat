cfedddf2b2ac867964905b171f7ee12ad33245f7
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-10 07:50:39 +0200

    .

---
+++ build.bat
@@ -0,0 +1,11 @@
+@setlocal
+@cd SLOTaxService
+@msbuild /t:build /p:Configuration=Release /nologo /v:m
+@if %errorlevel% neq 0 exit /b %errorlevel%
+@cd ..
+
+@cd SLOTaxGuiTest
+@msbuild /t:build /p:Configuration=Release /nologo /v:m
+@if %errorlevel% neq 0 exit /b %errorlevel%
+@cd ..
+
