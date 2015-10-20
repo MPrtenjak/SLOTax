d6965cd1b9acc29c6f4397b1da4dd8b8b91399bd
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-20 11:55:58 +0200

    Popravljen opis

--- SLOTaxGuiTest40/Properties/AssemblyInfo.cs
+++ SLOTaxGuiTest40/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SLOTaxGuiTest")]
+[assembly: AssemblyTitle("SLOTaxGuiTest40")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SLOTaxGuiTest")]
+[assembly: AssemblyProduct("SLOTaxGuiTest40")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
