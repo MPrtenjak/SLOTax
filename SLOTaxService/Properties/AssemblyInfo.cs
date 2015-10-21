f7d87ec8b6711a50ceb0a0505435295c3400b391
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-21 20:18:47 +0200

    Most of the code moved to shared assembly project

--- SLOTaxService/Properties/AssemblyInfo.cs
+++ SLOTaxService/Properties/AssemblyInfo.cs
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2f0a646-91f9-4ddb-aadd-f7a8c1d97fd7")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -31,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
---
+++ SLOTaxService/Services/SetCryptoConfig.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="SetCryptoConfig.cs" company="MNet">
+//     Copyright (c) Matjaz Prtenjak All rights reserved.
+// </copyright>
+// <author>Matjaz Prtenjak</author>
+//-----------------------------------------------------------------------
+
+using System.Deployment.Internal.CodeSigning;
+using System.Security.Cryptography;
+
+namespace MNet.SLOTaxService.Services
+{
+  internal class SetCryptoConfig
+  {
+    public static void SetAlgorithm()
+    {
+      CryptoConfig.AddAlgorithm(typeof(RSAPKCS1SHA256SignatureDescription), "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256");
+    }
+  }
+}
