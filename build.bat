8eaf29c30e3cda54e25f6db96169dcc45fbfda1c
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-21 20:13:46 +0200

    Build .Net 4.0 and .Net 4.5 projects

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @setlocal
-@cd SLOTaxService
+
+@cd SLOTaxGuiTest40
 @msbuild /t:build /p:Configuration=Release /nologo /v:m
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @cd ..
@@ -9,3 +10,20 @@
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @cd ..
 
+@rd FinalOutput /s /q
+@md FinalOutput
+@cd FinalOutput
+@md Net40
+@md Net45
+
+@cd ..
+
+@copy SLOTaxGuiTest40\bin\Release\*.exe FinalOutput\Net40
+@copy SLOTaxGuiTest40\bin\Release\*.dll FinalOutput\Net40
+@copy SLOTaxGuiTest40\bin\Release\*.config FinalOutput\Net40
+@del FinalOutput\Net40\*vshost*
+
+@copy SLOTaxGuiTest\bin\Release\*.exe FinalOutput\Net45
+@copy SLOTaxGuiTest\bin\Release\*.dll FinalOutput\Net45
+@copy SLOTaxGuiTest\bin\Release\*.config FinalOutput\Net45
+@del FinalOutput\Net45\*vshost*
