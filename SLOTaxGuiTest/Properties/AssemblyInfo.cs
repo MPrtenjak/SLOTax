7c439022c7fdd98f274e01fd5e1adc94f60bf551
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-23 22:06:15 +0200

    Versioning all assemblies at once

--- SLOTaxGuiTest/Properties/AssemblyInfo.cs
+++ SLOTaxGuiTest/Properties/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using System.Windows;
 [assembly: AssemblyTitle("SLOTaxGuiTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SLOTaxGuiTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -36,16 +32,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
