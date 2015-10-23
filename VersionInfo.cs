87eea05b30bd433e976330ab65c1b59f43b5d095
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-23 22:06:49 +0200

    Versioning all assemblies at once

---
+++ VersionInfo.cs
@@ -0,0 +1,25 @@
+// <copyright file="VersionInfo.cs" company="MNet">
+//     Copyright (c) Matjaz Prtenjak All rights reserved.
+// </copyright>
+// <author>Matjaz Prtenjak</author>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("Matjazev.NET")]
+[assembly: AssemblyCopyright("Copyright © Matjazev.NET. All rights reserved")]
+[assembly: AssemblyTrademark("MNet")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.296.7905")]
+[assembly: AssemblyFileVersion("1.1.296.7905")]
