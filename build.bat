862830bc39879af9fb188dcbaa83b7f792cd365c
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-23 22:04:35 +0200

    Only build not copy files

--- build.bat
+++ build.bat
@@ -9,21 +9,3 @@
 @msbuild /t:build /p:Configuration=Release /nologo /v:m
 @if %errorlevel% neq 0 exit /b %errorlevel%
 @cd ..
-
-@rd FinalOutput /s /q
-@md FinalOutput
-@cd FinalOutput
-@md Net40
-@md Net45
-
-@cd ..
-
-@copy SLOTaxGuiTest40\bin\Release\*.exe FinalOutput\Net40
-@copy SLOTaxGuiTest40\bin\Release\*.dll FinalOutput\Net40
-@copy SLOTaxGuiTest40\bin\Release\*.config FinalOutput\Net40
-@del FinalOutput\Net40\*vshost*
-
-@copy SLOTaxGuiTest\bin\Release\*.exe FinalOutput\Net45
-@copy SLOTaxGuiTest\bin\Release\*.dll FinalOutput\Net45
-@copy SLOTaxGuiTest\bin\Release\*.config FinalOutput\Net45
-@del FinalOutput\Net45\*vshost*
