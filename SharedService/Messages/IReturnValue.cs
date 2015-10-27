ee2b5de7757cc35cb17af8e0a37f95f163b738d4
Matjaz Prtenjak <matjaz@matjazev.net>
2015-10-27 12:57:25 +0100

    Extracted interface form concrete class ReturnValue

---
+++ SharedService/Messages/IReturnValue.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="IReturnValue.cs" company="MNet">
+//     Copyright (c) Matjaz Prtenjak All rights reserved.
+// </copyright>
+// <author>Matjaz Prtenjak</author>
+//-----------------------------------------------------------------------
+
+using System.Xml;
+using MNet.SLOTaxService.Services;
+
+namespace MNet.SLOTaxService.Messages
+{
+  public interface IReturnValue
+  {
+    BarCodes BarCodes { get; }
+    string ErrorMessage { get; }
+    ErrorMessageSource ErrorMessageSource { get; }
+    XmlDocument MessageReceivedFromFurs { get; }
+    XmlDocument MessageSendToFurs { get; }
+    MessageType MessageType { get; }
+    string ProtectedID { get; }
+    SendingStep Step { get; }
+    bool Success { get; }
+    string UniqueInvoiceID { get; }
+  }
+}
