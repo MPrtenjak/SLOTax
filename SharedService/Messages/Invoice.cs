d7642a7a654b6dfb7aee59cf4193fb22faf06b5f
Matjaz Prtenjak <matjaz@matjazev.net>
2015-11-25 12:10:19 +0100

    ProtectedID element reposition
    PreotecteID element is not the last element in Invoice element so we
    have to position it on the right place

--- SharedService/Messages/Invoice.cs
+++ SharedService/Messages/Invoice.cs
@@ -65,8 +65,31 @@ namespace MNet.SLOTaxService.Messages
         ProtectiveMark pm = new ProtectiveMark();
         string protectedIDValue = pm.Calculate(invoice as XmlElement, Settings.CryptoProvider);
 
-        invoice.AppendChild(XmlHelperFunctions.CreateElement(message, this.Settings.FursXmlNamespace, "ProtectedID", protectedIDValue));
+        // ProtectedID is not the last element in XML Schema, so we must put it in the right spot
+        //    we are going from bottom up, searching for the right spot
+        XmlNode protectedIDNode = XmlHelperFunctions.CreateElement(message, this.Settings.FursXmlNamespace, "ProtectedID", protectedIDValue);
+
+        XmlNode currentNode = invoice.LastChild;
+        while ((currentNode != null) && (this.isNodeAfterProtectedID(currentNode)))
+          currentNode = currentNode.PreviousSibling;
+
+        if (currentNode != null)
+          invoice.InsertAfter(protectedIDNode, currentNode);
       }
+    }
+
+    private bool isNodeAfterProtectedID(XmlNode node)
+    {
+      // nodes which are not elements (aka. spaces...)
+      if (node.NodeType != XmlNodeType.Element) return true;
+
+      // some nodes in Invoice element are after ProtectedID and it this is one of those nodes, function returns true
+      if (string.Compare(node.LocalName, "SubsequentSubmit", true) == 0) return true;
+      if (string.Compare(node.LocalName, "ReferenceInvoice", true) == 0) return true;
+      if (string.Compare(node.LocalName, "ReferenceSalesBook", true) == 0) return true;
+      if (string.Compare(node.LocalName, "SpecialNotes", true) == 0) return true;
+
+      return false;
     }
   }
 }
