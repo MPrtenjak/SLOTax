14ba3d7fabf0d61672ff6e86cf98d565ff174eae
Matjaž Prtenjak <matjaz@matjazev.net>
2017-01-02 09:33:43 +0100

    New Password window
    New window for setting passwords of certificates loaded from hard drive

---
+++ ShareGUI/PasswordBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="PasswordBox.xaml.cs" company="MNet">
+//     Copyright (c) Matjaz Prtenjak All rights reserved.
+// </copyright>
+// <author>Matjaz Prtenjak</author>
+//-----------------------------------------------------------------------
+
+using System.Windows;
+
+namespace MNet.SLOTaxGuiTest
+{
+  public partial class PasswordBox : Window
+  {
+    public string Password { get; set; }
+    public bool Commit { get; set; }
+
+    public PasswordBox()
+    {
+      this.InitializeComponent();
+      this.Commit = false;
+
+      this.passwordBox.Focus();
+    }
+
+    private void btnCancel(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void btnOK(object sender, RoutedEventArgs e)
+    {
+      this.Commit = true;
+      this.Password = this.passwordBox.Password;
+      this.Close();
+    }
+  }
+}
